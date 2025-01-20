d0ee360abb4017a151598b416ea44772cdbc1249
Atom M. Nava <atommunoznava@gmail.com>
2025-01-20 16:49:10 -0600

    Add files via upload

---
+++ 4user_input.py
@@ -0,0 +1,20 @@
+name = "Atom"
+age = 25
+gpa = 3.2
+is_student = True
+
+print(type(name))
+
+gpa = int(gpa)
+print(gpa)
+
+age = str(age)
+print(age)
+
+print(type(age))
+
+name1 = input("Name: ")
+age1 = input("Age: ")
+
+print(f"Hello {name1}!")
+print(f"You are {age1} yeasrs old")

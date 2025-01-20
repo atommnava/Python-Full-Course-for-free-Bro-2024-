b68fdc1b8459f0996f14a6a7bf01ce9c3fa93d30
Atom M. Nava <atommunoznava@gmail.com>
2025-01-20 16:59:31 -0600

    Update 4user_input.py

--- 4user_input.py
+++ 4user_input.py
@@ -18,3 +18,20 @@ age1 = input("Age: ")
 
 print(f"Hello {name1}!")
 print(f"You are {age1} yeasrs old")
+
+# Exercise 1 Rectangle Area Calc
+
+length = float(input("Enter the lenght: "))
+width = float(input("Enter the width: "))
+area = length * width
+print("Area is ", area)
+print(f"Area is {area}")
+
+# Exercise 2 Shopping cart program
+
+item = input("What item would you like to buy?")
+price = float(input("What is the price?"))
+quantity = int(input("How many would you like?"))
+total = price * quantity
+print(f"You have bought {quantity} x {item}/s")
+print(f"Your total is {total}")
